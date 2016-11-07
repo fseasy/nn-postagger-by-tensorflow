f43bf398283aefd5849145ad0328e5d1ef0ca2f1
memeda <readonlyfile@hotmail.com>
2016-11-07 18:31:43 +0800

    update reader

--- dataset_handler/reader.py
+++ dataset_handler/reader.py
@@ -4,26 +4,45 @@
 import os
 import gzip
 
-_data_dir = "../data_from_3rdparty/scir_training_day/3-nlp-practice"
+_cur_dir_path = os.path.split(os.path.realpath(__file__))[0]
+_data_dir = os.path.join(_cur_dir_path, "../data_from_3rdparty/scir_training_day/3-nlp-practice")
 
 _train_data_path = os.path.join(_data_dir, "penn.train.pos.gz")
 _devel_data_path = os.path.join(_data_dir, "penn.devel.pos.gz")
 _test_data_path = os.path.join(_data_dir, "penn.test.pos.blind.gz")
 
-def read_annotated_data(fpath):
-    with gzip.open(fpath, 'r', encoding="utf8") as f:
+def _read_annotated_data(fpath):
+    '''read annotated data. (from format 'word/tag ...)' => ( word_list, tag_list )'''
+    with gzip.open(fpath, 'rt', encoding="utf-8") as f:
         annotated_dataset = []
         for line in f:
             line = line.strip()
             word_tag_pair_list = line.split()
-            instance = [ [ pair[0] for pair in word_tag_pair_list  ], [ pair[1] for pair in word_tag_pair_list ]  ]
+            instance = ([], [])
+            for word_tag_pair in word_tag_pair_list:
+                (word, tag) = word_tag_pair.rsplit('/', 1) # may have multiple '/'
+                instance[0].append(word)
+                instance[1].append(tag)
             annotated_dataset.append(instance)
         return annotated_dataset
 
+def _read_unannotated_data(fpath):
+    '''read unannotated data. (from format 'word/_ ...') => word_list '''
+    with gzip.open(fpath, 'rt', encoding="utf-8") as f:
+        unannotated_dataset = []
+        for line in f:
+            line = line.strip()
+            word_tag_pair_list = line.split()
+            instance = [ pair.rsplit('/', 1)[0] for pair in word_tag_pair_list ]
+            unannotated_dataset.append(instance)
+        return unannotated_dataset
+
+
 def read_training_data():
-    return read_annotated_data(_train_data_path)
+    return _read_annotated_data(_train_data_path)
 
 def read_devel_data():
-    return read_annotated_data(_devel_data_path)
+    return _read_annotated_data(_devel_data_path)
 
-def read_
+def read_test_data():
+    return _read_unannotated_data(_test_data_path)
