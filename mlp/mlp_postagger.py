1c807e2f83511ee26cd2eeea5878d525b58935f2
memeda <readOnlyFile@hotmail.com>
2016-11-15 12:59:41 +0800

    mlp(46%)

--- mlp/mlp_postagger.py
+++ mlp/mlp_postagger.py
@@ -11,16 +11,22 @@ sys.path.append(_cur_dir)
 
 from mlp_data import MlpData, RandomSeed
 
-def MlpNet(object):
-    
+class MlpNet(object):
     def __init__(self):
         self.graph = tf.Graph()
         self.sess = tf.Session()
-
-    def build_net(self, batch_sz, window_sz, worddict_sz, embedding_dim, hidden_dim, tagdict_sz):
+    
+    def build_input(self, batch_sz, window_sz):
         with self.graph.as_default(), tf.device("/cpu:0"):
-            with tf.name_scope("input")
+            with tf.name_scope("input"):
                 batch_x_input = tf.placeholder(tf.int32, shape=(batch_sz, window_sz))
+                batch_y_input = tf.placeholder(tf.int32, shape=(batch_sz,))
+            # export
+            self.batch_x_input = batch_x_input
+            self.batch_y_input = batch_y_input
+
+    def build_logit(self, batch_sz, window_sz, worddict_sz, embedding_dim, hidden_dim, tagdict_sz):
+        with self.graph.as_default(), tf.device("/cpu:0"):
             with tf.name_scope("project"):
                 embedding_table = tf.Variable( 
                                     tf.random_uniform( (worddict_sz, embedding_dim), -1., 1. ),
@@ -50,23 +56,25 @@ def MlpNet(object):
                             tf.zeros( (tagdict_sz,) ),
                             name="b_softmax"
                         )
-                batch_logits = tf.matmul(batch_hidden_out, w_o,transpose_b=True) + b_o
-            with tf.name_scope("loss"):
-                batch_y_input = tf.placeholder(tf.int32, shape=(batch_sz,))
-                batch_loss = tf.sparse_softmax_cross_entropy_with_logits(batch_logits, batch_y_input, name="batch_loss")
-                loss = tf.reduce_mean(batch_loss, name="loss")
+                batch_logit = tf.matmul(batch_hidden_out, w_o,transpose_b=True) + b_o
+           
             # export the symbol-variables
-            self.batch_logits = batch_logits # for eval or predict
-            self.loss = loss
+            self.batch_logit = batch_logit # for eval or predict
             self.batch_x_input = batch_x_input
-            self.batch_y_input = batch_y_input
             
         def build_train_op(self, learning_rate):
+            with tf.name_scope("loss"):
+                batch_loss = tf.sparse_softmax_cross_entropy_with_logits(batch_logits, batch_y_input, name="batch_loss")
+                loss = tf.reduce_mean(batch_loss, name="loss")
             tf.summary.scalar('loss', self.loss, name="loss_summary", description="test for loss summary")
             optimizer  = tf.train.GradientDescentOptimizer(learning_rate)
             global_step = tf.Variable(0, name="global_step", trainable=False) # record how much steps updated.
-            train_op = optimizer.minimize(self.loss, global_step=global_step)
+            train_op = optimizer.minimize(loss, global_step=global_step)
             # export the symbol-variable
+            self.batch_y_input = batch_y_input
             self.train_op = train_op
+
+        def build_eval_op(self):
+            
         
 
